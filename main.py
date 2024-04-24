83c819b3ab43ced60c3bbb3762153fd977d1d58f
Sergey Workstation <s.v.brovko@yandex.ru>
2024-04-24 11:12:31 +0700

    Все еще медленно

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import re
 def find_sub_qty(string, sub):
     count = 0
     for x in range(len(string) - len(sub) +1):
@@ -9,25 +8,21 @@ def find_sub_qty(string, sub):
 def minion_game(string):
     string = string.strip()
     Vowels = 'AEIOU'
-    stuart = {}
-    kevin = {}
-    for i in range(1,len(string)+1):
-        for j in range(len(string)):
-            sub_string = string[j:j+i]
-            if (sub_string not in stuart) and (string[j] not in Vowels):
-                stuart[sub_string] = find_sub_qty(string, sub_string)
-            elif (sub_string not in kevin) and (string[j] in Vowels):
-                kevin[sub_string] = find_sub_qty(string, sub_string)
-    score_stuart = sum(stuart.values())
-    score_kevin = sum(kevin.values())
+    stuart = set()
+    kevin = set()
+    for i in range(len(string)):
+        for j in range(len(string)-i):
+            sub_string = string[j:j+i+1]
+            if string[j] not in Vowels:
+                stuart.add(sub_string)
+            else:
+                kevin.add(sub_string)
+    score_stuart = sum([ find_sub_qty(string, sub) for sub in stuart])
+    score_kevin = sum([ find_sub_qty(string, sub) for sub in kevin])
     if score_stuart > score_kevin:
         print (f"Stuart {score_stuart}")
     if score_stuart < score_kevin:
         print (f"Kevin {score_kevin}")
     if score_stuart == score_kevin:
         print ('Draw')
-# patern = re.compile('ANA')
-# print(patern.search('BANANA').span()[0])
-# print(patern.search('BANANA', 2))
-# print (find_sub_qty('BANANA', 'ANA'))
 minion_game('BANANA')
